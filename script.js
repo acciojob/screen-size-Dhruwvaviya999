d2d0401a24a47f96fce53aa079cf7ac774d152e4
Dhruw Vaviya <dhruwvaviya123@gmail.com>
2025-04-02 15:42:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+
+let div = document.getElementById("sizeInfo");
+let h1 = div.children[0];
+
+window.addEventListener("load", (e) => {
+	h1.textContent = `Width: ${window.innerWidth} and Height: ${window.innerHeight}`;
+})
+
+window.addEventListener("resize", (e) => {
+	h1.textContent = `Width: ${window.innerWidth} and Height: ${window.innerHeight}`;
+})
